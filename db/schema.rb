8c70dbf37227c88bc044d6a68237050fb0c49c8b
Paul K <pkfis@outlook.com>
2020-01-29 03:57:36 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20200129035601) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.float "balance"
+  end
+
 end
